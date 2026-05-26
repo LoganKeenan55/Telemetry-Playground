64e69fc91d75449d104d99e39b08d013b3ed136a
Logan <logankeenan55@gmail.com>
2026-05-26 14:32:40 -0400

    Refactored Code to use SSE

--- telemetry/main.go
+++ telemetry/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
 	"time"
@@ -37,10 +38,50 @@ func handler(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-type","application/json");
 	json.NewEncoder(w).Encode(payload);
 	
+}
+
+func eventsHandler(w http.ResponseWriter, r *http.Request){
+
+	//needed SSE headers to keep connection alive
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+
+
+	//flusher is made so updates can be sent immediately
+	flusher := w.(http.Flusher)
+	for{
+		//content = byte[], error = error
+		content, error := os.ReadFile(filePath)
+
+		if error != nil{
+			http.Error(w, "File could not be read",1);
+			return;
+		}
+
+		//converts JSON into payload struct
+		var payload Payload
+		if error := json.Unmarshal(content,&payload); error != nil{
+			http.Error(w,"JSON could not be parsed",1);
+			return;
+		}
+
+		//converts struct back into JSON
+		jsonData, error := json.Marshal(payload);
+
+		if error!= nil{
+			http.Error(w, "Struct couldn't be turned into JSON",1);
+		}
+
+		fmt.Fprintf(w, "data: %s\n\n", jsonData)
+
+		flusher.Flush();
+
+		time.Sleep(1*time.Second);
+	}
 
 
 }
-
 
 
 
@@ -55,13 +96,11 @@ func main() {
 		}
 	}()
 
-
-	http.HandleFunc("/data", handler)
+	// /data for request/repsonse, /events for open connection
+	http.HandleFunc("/events", eventsHandler)
 
 	http.Handle("/", http.FileServer(http.Dir(".")))
 	
-	http.ListenAndServe(":8080", nil)
-
 	err := http.ListenAndServe(":8080", nil)
 
 	if err != nil {
