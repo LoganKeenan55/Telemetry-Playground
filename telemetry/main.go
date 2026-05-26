2254eccba9cd65861a2c918baea2e0807ac7f3c5
Logan <logankeenan55@gmail.com>
2026-05-26 12:20:50 -0400

    Added Payload struct and a handler to print it to the 8080 local host

--- telemetry/main.go
+++ telemetry/main.go
@@ -1,18 +1,62 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
+	"os"
 )
+
+//Notes: handleFunc takes a handler function that is run when connecting to server
+//JSON fields need to be capital letter
+//float -> float64
+
+var filePath string = "payload.json"
+
+
+type Payload struct{
+
+  TemperatureC float64 `json:"temperatureC"`
+  VoltageV float64 `json:"voltageV"`
+  PowerW float64 `json:"powerW"`
+  HfPingRateHz float64 `json:"hfPingRateHz"`
+  LfPingRateHz float64 `json:"lfPingRateHz"`
+  TriggerRateHz float64 `json:"triggerRateHz"`
+  Timestamp string `json:"timestamp"`
+
+}
+
+
+
+func handler(w http.ResponseWriter, r *http.Request){
+	//content = byte[], error = error
+	content, error := os.ReadFile(filePath)
+
+	if error != nil{
+		http.Error(w, "File could not be read",1);
+		return;
+	}
+
+	var payload Payload
+	if error := json.Unmarshal(content,&payload); error != nil{
+		http.Error(w,"JSON could not be parsed",1);
+		return;
+	}
+
+	//send JSON to browser
+
+	w.Header().Set("Content-type","application/json");
+	json.NewEncoder(w).Encode(payload);
+	
+
+
+}
+
+
+
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, HTTP!")
-	})
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Println(err)
-	}
 }
